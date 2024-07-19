d9bcd804e46732df4d516c292566675ad161d583
Marie Idleman <marie@iamfurious.com>
2024-07-19 07:23:09 -0500

    add test

---
+++ pages/projectPage.ts
@@ -0,0 +1,63 @@
+import { Page, expect } from "@playwright/test";
+import { BasePage } from "./basePage";
+
+export class ProjectPage extends BasePage {
+  private iframe = this.page.frameLocator('iframe[title="Untitled Project"]');
+
+  super(page: Page) {
+    this.page = page;
+  }
+
+  /**
+   * Verifies message that project is deploying is visible
+   */
+  async verifyIsDeploying() {
+    await expect(this.page.getByText("Deploying Project")).toBeVisible();
+  }
+
+  /**
+   * Verifies all sections of the project have loaded as expected
+   */
+  async verifyHasDeployed() {
+    await this.verifyHeader();
+    await this.verifyIdeSection();
+    await this.verifyDetailSection();
+    await this.verifyFileSection();
+  }
+
+  /**
+   * Verifies the default header of the project displays
+   */
+  async verifyHeader() {
+    await expect(this.page.getByText("Click to name your project")).toBeVisible(
+      {
+        timeout: 45000,
+      }
+    );
+  }
+
+  /**
+   * Verifies the IDE frame of the project displays
+   */
+  async verifyIdeSection() {
+    await expect(this.iframe.getByLabel("ConsoleTabSet")).toBeVisible({
+      timeout: 20000,
+    });
+    await expect(this.iframe.getByText("Terminal")).toBeVisible();
+    await expect(this.iframe.getByText("Background Jobs")).toBeVisible();
+  }
+
+  /**
+   * Verifies the detail section of the project displays
+   */
+  async verifyDetailSection() {
+    // to-do: implement
+  }
+
+  /**
+   * Verifies the file section of the project displays
+   */
+  async verifyFileSection() {
+    // to-do: implement
+  }
+}
---
+++ tests/projects/createProject.test.ts
@@ -0,0 +1,34 @@
+import { test } from "@playwright/test";
+import { LoginPage } from "../../pages/loginPage";
+import { SidePanel } from "../../pages/sidePanel";
+import { SpacePage } from "../../pages/spacePage";
+import { ProjectPage } from "../../pages/projectPage";
+
+test.describe("Project", () => {
+  test.beforeEach(async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const sidePanel = new SidePanel(page);
+
+    await loginPage.login();
+    await sidePanel.deleteAllSpaces();
+  });
+
+  test(
+    "should be able to create a new project",
+    {
+      tag: ["@smoke"],
+    },
+    async ({ page }) => {
+      // create a new project within a new space
+      const sidePanel = new SidePanel(page);
+      await sidePanel.createNewSpace();
+      const space = new SpacePage(page);
+      await space.createNewProject();
+
+      // verify the project has successfully deployed
+      const project = new ProjectPage(page);
+      await project.verifyIsDeploying();
+      await project.verifyHasDeployed();
+    }
+  );
+});
